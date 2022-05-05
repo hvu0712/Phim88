f368e4029e5397983b2b0213b7ff680b2b5fac3e
Thu Phương <phamlongvu@Phams-MacBook-Pro.local>
2022-05-05 09:20:53 +0700

    add splash screen

--- app/src/main/java/com/example/phim88/views/SplashScreen.java
+++ app/src/main/java/com/example/phim88/views/SplashScreen.java
@@ -1,14 +1,16 @@
-package com.example.phim88;
+package com.example.phim88.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.phim88.R;
+
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
